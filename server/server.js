d4a4dc9631746d1bd412c0e27578d25189172e0f
Santiago Suárez <santiago.su.mo@gmail.com>
2015-11-02 18:32:26 -0800

    Serve client

--- server/server.js
+++ server/server.js
@@ -1,8 +1,10 @@
 var express = require('../node_modules/express');
 var app = express();
 
+app.use(express.static('./client'));
+
 app.get('/', function (req, res) {
-  res.send('Hello World!');
+  res.sendFile('./client/index.html');
 });
 
 var server = app.listen(3000, function () {
